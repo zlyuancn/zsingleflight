4e8984145c5bb6b578dd66a5f732ff6ec04f7637
zlyuancn <zlyuancn@163.com>
2020-05-29 11:29:43 +0800

    添加DoHashKey测试

--- singleflight_test.go
+++ singleflight_test.go
@@ -57,7 +57,7 @@ func TestDo(t *testing.T) {
     }
 }
 
-func Benchmark_A(b *testing.B) {
+func Benchmark_Do(b *testing.B) {
     var sf = New()
 
     b.ResetTimer()
@@ -70,6 +70,26 @@ func Benchmark_A(b *testing.B) {
             m := n.Int64()
 
             _, _ = sf.Do(strconv.Itoa(int(m)), func() (i interface{}, e error) {
+                return nil, nil
+            })
+            i++
+        }
+    })
+}
+
+func Benchmark_DoHashKey(b *testing.B) {
+    var sf = New()
+
+    b.ResetTimer()
+
+    b.RunParallel(func(p *testing.PB) {
+        i := 0
+        for p.Next() {
+            sr := new(big.Int).SetInt64(10000)
+            n, _ := rand.Int(rand.Reader, sr)
+            m := n.Int64()
+
+            _, _ = sf.DoHashKey(uint64(m), func() (i interface{}, e error) {
                 return nil, nil
             })
             i++
